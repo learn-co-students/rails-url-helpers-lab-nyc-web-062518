5a3bc7377f9445a3fb316a2e1e7d095e85a8c931
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-22 10:34:53 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find_by(id: params[:id])
+    if @student.active == true
+    @status = "active"
+    else
+    @status = "inactive"
+    end
+  end
+
+  def activate
+    @student = Student.find_by(id: params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
@@ -13,4 +27,5 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
--- config.ru
+++ config.ru
@@ -2,3 +2,8 @@
 
 require ::File.expand_path('../config/environment', __FILE__)
 run Rails.application
+
+# def index
+#   @tasks = Task.all
+#   byebug
+# end
